8cc03b95f2191c1a40bf3d521e31cb95b086273b
MSun <msun@MM63768M1.local>
2015-10-30 17:13:47 -0400

    merge branches

--- browser/js/common/directives/oauth-button/oauth-button.directive.js
+++ browser/js/common/directives/oauth-button/oauth-button.directive.js
@@ -6,6 +6,14 @@ app.directive('oauthButton', function () {
       providerName: '@'
     },
     restrict: 'E',
-    templateUrl: 'js/common/directives/oauth-button/oauth-button.html'
+    templateUrl: 'js/common/directives/oauth-button/oauth-button.html',
+      controller: function($scope){
+          console.log($scope.providerName);
+          $scope.imgLink = {
+              'Google': 'https://developers.google.com/+/images/branding/sign-in-buttons/Red-signin_Google_base_44dp.png',
+              'Facebook': 'http://i.stack.imgur.com/pZzc4.png'
+          };
+
+      }
   }
 });
